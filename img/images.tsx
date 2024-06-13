5a86420b911bfec3b381586f87c1d6e7b30d428d
Esvol <o.voloshyn@obdarovanist.kh.ua>
2024-06-13 13:23:39 +0300

    Update for the storage page

--- img/images.tsx
+++ img/images.tsx
@@ -6,6 +6,8 @@ import storageCap from './storage/storageCap.svg'
 import storageShirt from './storage/storageShirt.svg'
 import storageTrousers from './storage/storageTrousers.svg'
 import storageSneakers from './storage/storageSneakers.svg'
+import storageLine from './storage/storageLine.svg'
+import arrow from './storage/arrow.svg'
 
 import timer from './timer.svg'
 import cap1 from './1lvl/cap1.svg'
@@ -13,4 +15,4 @@ import tshirt1 from './1lvl/tshirt1.svg'
 import trousers1 from './1lvl/trousers1.svg'
 import sneakers1 from './1lvl/sneakers1.svg'
 
-export {empty_avatar, coin, mannequin, storageCap, storageShirt, storageTrousers, storageSneakers, timer, cap1, tshirt1, trousers1, sneakers1,}
+export {empty_avatar, coin, mannequin, storageCap, storageShirt, storageTrousers, storageSneakers, storageLine, arrow, timer, cap1, tshirt1, trousers1, sneakers1,}
--- src/app/storage/page.tsx
+++ src/app/storage/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './index.module.scss'
 import StorageCard from '../../../components/StorageCard'
 import Image from 'next/image'
-import { sneakers1, storageCap, storageShirt, storageSneakers, storageTrousers, timer, } from '../../../img/images'
+import { arrow, coin, sneakers1, storageCap, storageLine, storageShirt, storageSneakers, storageTrousers, timer, } from '../../../img/images'
 
 import { FaArrowRightLong } from "react-icons/fa6";
 
@@ -26,6 +26,59 @@ const nav = [
   {
     img: timer,
     title: "Time"
+  },
+]
+
+const storageCard = [
+  {
+    level: 1,
+    speed: 0.24,
+    title: 'Basic Green',
+    headline: '',
+    text: '',
+    image: sneakers1,
+    purchased: true,
+    locked: false,
+  },
+  {
+    level: 2,
+    speed: 0.28,
+    title: 'Royal Sapphire',
+    headline: '',
+    text: '',
+    image: sneakers1,
+    purchased: false,
+    locked: false,
+  },
+  {
+    level: 3,
+    speed: 0.33,
+    title: 'Gleaming Emerald',
+    headline: '',
+    text: '',
+    image: sneakers1,
+    purchased: false,
+    locked: true,
+  },
+  {
+    level: 4,
+    speed: 0.38,
+    title: 'Radiant Ruby',
+    headline: '',
+    text: '',
+    image: sneakers1,
+    purchased: false,
+    locked: true,
+  },
+  {
+    level: 5,
+    speed: 0.45,
+    title: 'Epic Galaxy',
+    headline: '',
+    text: '',
+    image: sneakers1,
+    purchased: false,
+    locked: true,
   },
 ]
 
@@ -57,7 +110,10 @@ const Storage = () => {
               Ideal for everyday wear, but not intended for intense sports or long-term use.
             </p>
             <div className={styles.storage_container_clothes_info_upgrade}>
-              <p className={styles.storage_container_clothes_info_upgrade_price}>140</p>
+              <p className={styles.storage_container_clothes_info_upgrade_price}>
+                <Image src={coin} alt='coin' width={20} height={20}/>
+                140
+              </p>
               <button>Upgrade</button>
             </div>
           </div>
@@ -65,28 +121,30 @@ const Storage = () => {
           <div className={styles.storage_container_clothes_details}>
             <div className={styles.storage_container_clothes_details_box}>
               <div className={styles.storage_container_clothes_details_box_was}>
-                <p>0.25 coins</p> 
-                <p>1 hours</p>
+                <div>0.25 coins</div> 
               </div>
-              <FaArrowRightLong fontSize={60} color='black'/>
+              <Image src={arrow} width={80} height={80} alt='arrow' />
+              {/* <FaArrowRightLong fontSize={60} color='black'/> */}
               <div className={styles.storage_container_clothes_details_box_will}>
-                <p>0.29 coins</p> 
-                <p>1 hours</p>
+                <div>0.29 coins</div> 
               </div>  
             </div>
           </div>
         </div>
-        {/* <h1>Clothes</h1>
-        <div className={styles.storage_container_clothes}>
-          <div className={styles.storage_container_clothes_main}>
-            <Image src={sneakers1} alt='' width={120} height={120}/>
-            <p>Green</p>
-          </div>
-          <StorageCard />
-          <StorageCard />
-          <StorageCard />
-          <StorageCard />
-        </div> */}
+
+        <div className={styles.storage_container_upgrade}>
+          {
+            storageCard.map((card, index) => (
+              <>
+                <StorageCard speed={card.speed} level={card.level} image={card.image} title={card.title} purchased={card.purchased} locked={card.locked}/>
+                {
+                  index !== (storageCard.length - 1) 
+                    && <Image src={storageLine} alt='line' width={20} height={20} style={{transform: `${index % 2 == 1 ? 'scaleY(-1)' : ''}`}}/>
+                }
+              </>
+            ))
+          }
+        </div>
       </div>
       
       <div className={styles.storage_container}>
